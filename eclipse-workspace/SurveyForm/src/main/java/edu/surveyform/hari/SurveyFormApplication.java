6d13d2c6f1966052f2bbd148a68f76f0525f3da1
hariTH-0828 <hariharan08112001@gmail.com>
2022-06-09 18:30:47 +0530

    Initial Updates

---
+++ eclipse-workspace/SurveyForm/src/main/java/edu/surveyform/hari/SurveyFormApplication.java
@@ -0,0 +1,13 @@
+package edu.surveyform.hari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyFormApplication.class, args);
+	}
+
+}
